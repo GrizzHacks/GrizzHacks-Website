221b05b15d0e537e15e50ab9fd31bb03dc66c451
AND <arughani@intrepidcs.com>
2017-07-24 03:08:00 -0400

    GrizzHacks 2
    GrizzHacks 2 June update

--- Front End/js/main.js
+++ Front End/js/main.js
@@ -210,3 +210,13 @@
 
 
 }());
+
+//faq toggle stuff 
+$('.togglefaq').click(function(e) {
+e.preventDefault();
+var notthis = $('.active').not(this);
+notthis.find('.icon-minus').addClass('icon-plus').removeClass('icon-minus');
+notthis.toggleClass('active').next('.faqanswer').slideToggle(300);
+ $(this).toggleClass('active').next().slideToggle("fast");
+$(this).children('i').toggleClass('icon-plus icon-minus');
+});
